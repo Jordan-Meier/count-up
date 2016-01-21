ec14884e299071b5dae734adabb423e5c2fcc765
jordan meier and sara wagner <sara.wagner23q@gmail.comx>
2016-01-21 09:14:55 -0800

    Add functionality to test 1, so that it will count to a given number

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,6 @@
-var leapYear = function(year) {
-  //Lines 2-6 are not necessary for functionality because everything that's not a leap year is returned as false. Lines are used for readability and logic
-  if (parseInt(year) === NaN) {
-    return false;
-  } else if (year === "september") {
-    return false;
-  } else if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+var countUp = function(number) {
+  for (var i = 1; i <= number; i+=1) {
     return true;
-  } else {
-    return false;
   }
-};
+
+}
