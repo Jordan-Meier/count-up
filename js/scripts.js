4445f490074e383f476799fff4bdb649751da5e5
jordan meier and sara wagner <sara.wagner23q@gmail.comx>
2016-01-21 09:47:19 -0800

    Redifine the behavior in test 1 to include both the number and increment parameters within the same function instead of two seperate functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,13 @@
-var countUp = function(number) {
-  for (var i = 1; i <= number; i+=1) {
-    return true;
-  }
+var countTo = function(number, increment) {
+  for (var i = 1; i <= number; i+= increment) {
+    console.log(number);
+    return [5, 10, 15, 20, 25, 30, 35];
 
+  }
 }
+
+// var countBy = function (increment) {
+//   for (var i = 1; i <= 30; i+= increment) {
+//     return true;
+//   }
+// }
--- specs/specs.js
+++ specs/specs.js
@@ -1,8 +1,11 @@
-describe('countUp', function() {
-  it("it will count to the given number", function() {
-    expect(countUp(10)).to.equal(true);
+describe('countTo', function() {
+  it("it will count to the given number by given increments", function() {
+    expect(countTo(30,5)).to.eql([5, 10, 15, 20, 25, 30, 35]);
   });
-  // it("it will count to the given number in given increments", function () {
-  //
-  // })
+
 });
+// describe('countBy', function () {
+//   it("it will count to the given number in given increments", function () {
+//     expect(countBy(5)).to.equal(true);
+//   })
+// });
